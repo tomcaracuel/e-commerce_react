313d6232bb8dbc21c59ec4785092f2fca76c6d94
tomcaracuel <tfcaracuel@gmail.com>
2023-05-25 22:02:30 -0300

    product page design

--- client/src/pages/Product/Product.jsx
+++ client/src/pages/Product/Product.jsx
@@ -1,9 +1,71 @@
 import React from 'react'
+import { useState } from 'react'
 import './Product.scss'
+import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart"
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder"
+import BalanceIcon from "@mui/icons-material/Balance"
 
 const Product = () => {
+
+  const [selectedImg, setSelectedImg] = useState(0)
+  const [quantity, setQuantity] = useState(1)
+  
+  const images = [
+    "https://img.freepik.com/free-photo/green-sofa-white-living-room-with-free-space_43614-834.jpg?w=1380&t=st=1685045049~exp=1685045649~hmac=e55b1e03f0de373bfc22962846eadd8b6539fde5131b1c4ecc3c434cdda2bbf8",
+    "https://img.freepik.com/free-psd/modern-interior-design-living-room_176382-1266.jpg?w=826&t=st=1685045076~exp=1685045676~hmac=590ed85acc9bdd4f9c75837166bea89095cbdbc33fb011feda897b28fe3f710e"
+  ]
+
+
   return (
-    <div className='product'>Product</div>
+    <div className='product'>
+      <div className="left">
+        <div className="images">
+          <img src={images[0]} alt="" onClick={e=>setSelectedImg(0)}/>
+          <img src={images[1]} alt="" onClick={e=>setSelectedImg(1)}/>
+        </div>
+        <div className="mainImg">
+          <img src={images[selectedImg]} alt="" />
+        </div>
+      </div>
+      <div className="right">
+        <h1>Title</h1>
+        <span className='price'>$199</span>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad nam dolores at quod ut. Architecto, deleniti similique soluta quidem voluptate explicabo! Impedit saepe temporibus aut rerum commodi esse quod quos!
+        </p>
+        <div className="quantity">
+          <button onClick={()=>setQuantity((prev) =>  prev ===1 ? 1 : prev-1)}>-</button>
+          {quantity}
+          <button onClick={()=>setQuantity((prev) => prev+1)}>+</button>
+        </div>
+        <button className="add">
+          <AddShoppingCartIcon/>ADD TO CART
+        </button>
+        <div className="links">
+          <div className="item">
+            <FavoriteBorderIcon /> ADD TO WISH LIST
+          </div>
+          <div className="item">
+            <BalanceIcon /> ADD TO COMPARE
+          </div>
+        </div>
+        <div className="info">
+          <span>Vendor: Polo</span>
+          <span>Product Type: T-Shirt</span>
+          <span>Tag: T-Shirt, Women, Top</span>
+        </div>
+        <hr />
+        <div className="info">
+          <span>DESCRIPTION</span>
+          <hr />
+          <span>ADDITIONAL INFORMATION</span>
+          <hr />
+          <span>FAQ</span>
+        </div>
+
+
+      </div>
+    </div>
   )
 }
 
